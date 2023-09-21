a234952fea9138ae0335d530450b3922dfeb7ec8
Ernest <ests@users.noreply.github.com>
2023-09-21 20:34:50 +0200

    Parse bulk strings

--- src/resp.rs
+++ src/resp.rs
@@ -2,6 +2,7 @@
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::bytes::complete::tag_no_case;
+use nom::character::complete::u32;
 use nom::character::complete::alpha1;
 use nom::character::complete::digit1;
 use nom::character::complete::line_ending;
@@ -9,13 +10,26 @@ use nom::character::complete::{char, one_of};
 use nom::combinator::map_res;
 use nom::combinator::opt;
 use nom::combinator::recognize;
+use nom::bytes::complete::take;
 use nom::sequence::delimited;
+use nom::sequence::terminated;
 use nom::sequence::tuple;
 use nom::IResult;
 
 #[allow(unused)]
+pub fn parse_error(input: &str) -> IResult<&str, &str> {
+    delimited(char('-'), alpha1, line_ending)(input)
+}
+
+#[allow(unused)]
 pub fn parse_string(input: &str) -> IResult<&str, &str> {
     delimited(char('+'), alpha1, line_ending)(input)
+}
+
+#[allow(unused)]
+pub fn parse_bulk_string(input: &str) -> IResult<&str, &str> {
+    let (rest, length) = delimited(char('$'), u32, line_ending)(input)?;
+    terminated(take(length), line_ending)(rest)
 }
 
 #[allow(unused)]
@@ -39,6 +53,13 @@ mod tests {
         let (remaining_input, output) = parse_string("+OK\r\n").unwrap();
         assert_eq!(remaining_input, "");
         assert_eq!(output, "OK");
+    }
+
+    #[test]
+    fn test_parse_bulk_string() {
+        let (remaining_input, output) = parse_bulk_string("$7\r\nabc\r\n89\r\n").unwrap();
+        assert_eq!(remaining_input, "");
+        assert_eq!(output, "abc\r\n89");
     }
 
     #[test]
